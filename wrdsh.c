c948ac9304d67e2dbd9a89fe2ed52f14596609d0
Laurence Garcia <lsg913@tux8.usask.ca>
2021-10-01 15:35:24 -0600

    New version of wrdsh that now read the command line arguments from right to left. Still has major bugs, where the filename is being included when doing stdout redirection. Some of the code were also hardcoded, so expect so bugs when multiple redirection is given.

--- wrdsh.c
+++ wrdsh.c
@@ -6,22 +6,24 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 
 #define MAX_SIZE 256
 
 int main(int argc, char* argv[]){
     char *toks[MAX_SIZE];
     char cmd[MAX_SIZE];
-    int i, redirIN, redirOUT;
+    char *rcmd[MAX_SIZE];
+    char *newcmd[MAX_SIZE];
+    int i, redirIN, redirOUT, index,j;
     int fd[2];
-    
+
     while(1){
 
         // Prompt and get the command
         printf("wrdsh> ");
         fgets(cmd, 256, stdin);
         // printf("Arguments given: %s", cmd);
+        
         cmd[strlen(cmd) - 1] = 0;
 
         if (strcmp("", cmd) == 0 ){
@@ -35,23 +37,44 @@ int main(int argc, char* argv[]){
         // Convert command into tokens
         redirIN = 0;
         redirOUT = 0;
+        index = 0;
         i = 0;
-        char* tok = strtok(cmd, " ");
+        j=0;
+        char *tok = strtok(cmd, " ");
+        rcmd[index] = tok;
+        index++;
         while(tok != NULL){
             toks[i] = tok;
 
-            if (strcmp("<", tok) == 0){
+            if (strcmp(">", tok) == 0){
                 redirIN = i;
                 i--;
             }
-            else if (strcmp(">", tok) == 0){
+            else if (strcmp("<", tok) == 0){
                 redirOUT = i;
                 i--;
             }
             i++;
             tok = strtok(NULL, " ");
+            index++;
         }
+        index--;
+
+        if (!argc <= 1){
+            for (j=index-1; j>=0; j--){
+                newcmd[j] = toks[j];
+            }
+
+            char *temp = newcmd[1];
+            char *temp2 = newcmd[0];
+            newcmd[0] = temp;
+            newcmd[1] = newcmd[2];
+            newcmd[2] = temp2;
+        }
+
         toks[i] = NULL;
+        newcmd[i] = NULL;
+
 
         if (pipe(fd) == -1){
             return 1;
@@ -59,40 +82,50 @@ int main(int argc, char* argv[]){
         
         pid_t pid = fork();
         
-            if (pid < 0){
-                printf("Failed to create a child.");
-                exit(1);
-            }
-            else if(pid == 0 ){
-                if (redirOUT){
-                    close(fd[0]);
-                    if ((fd[1] = open(toks[redirOUT], O_CREAT | O_RDWR | O_APPEND, S_IRUSR | S_IWUSR)) == -1){
-                        perror(toks[redirOUT]);
-                        exit(EXIT_FAILURE);
-                    }
-                    dup2(fd[1], 1);
+        if (pid < 0){
+            printf("Failed to create a child.");
+                xit(EXIT_FAILURE);
+        }
+        else if(pid == 0 ){
+            if (redirOUT){
+                close(fd[0]);
+                redirOUT = 0;
+                if((fd[1] = open(toks[redirOUT], O_WRONLY | O_CREAT, 0777)) == -1){
+                    perror(toks[redirOUT]);
+                    exit(EXIT_FAILURE);
+                }
+                    dup2(fd[1], STDOUT_FILENO);
                     close(fd[1]);
                     toks[redirOUT] = NULL;
                 }
                 else if (redirIN){
-                    close(fd[1]);
+                    redirIN = 0;
                     if ((fd[0] = open(toks[redirIN], O_RDONLY, 0)) == -1){
                         perror(toks[redirIN]);
                         exit(EXIT_FAILURE);
                     }
 
-                    dup2(fd[0], 0);
+                    dup2(fd[0], STDIN_FILENO);
                     close(fd[0]);
                     toks[redirIN] = NULL;
                 }
 
-                execvp(toks[0], toks);
-                printf("wrdsh: %s: %s\n", toks[0], strerror(errno));
-                exit(1);
+
+                if (i > 2){
+                    execvp(newcmd[0], newcmd);
+                    printf("wrdsh: %s: command not found\n", newcmd[0]);
+                    exit(1);
+                }
+                else{
+                    execvp(toks[0], toks);
+                    printf("wrdsh: %s: command not found\n", toks[0]);
+                    exit(1);
+                }
             }
             else {
                 wait(NULL);
                 // printf("Child Exited\n");
+                
             }
     }
     return 0;
